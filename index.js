49f0544cedebc54756df692d0fac7419f6c9c992
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2023-05-16 09:15:21 -0700

    send all jobs, and filter jobs and specific job details function added

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 const app = express();
 
-// midlewire 
+// midlewire
 app.use(cors());
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kgqetuh.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,33 +18,49 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const jobCollection = client.db('jobPortalDB').collection('jobs');
+    const jobCollection = client.db("jobPortalDB").collection("jobs");
 
-    app.get('/allJobs', async(req, res)=>{
-        const result = await jobCollection.find().toArray();
-        res.send(result)
+    app.get("/allJobs/:status", async (req, res) => {
+      const status = req.params.status;
+
+      if (status == "Onsite" || status == "Remote") {
+        const query = { status: status}
+        const result = await jobCollection.find(query).toArray();
+        return res.send(result);
+      }
+
+      const result = await jobCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get('/job_details/:id', async(req, res)=>{
+      const id = req.params.id;
+  
+      const query = { _id : new ObjectId(id)}
+      const result = await jobCollection.findOne(query);
+      res.send(result);
     })
 
+    app.post("/postJob", async (req, res) => {
+      const jobDetails = req.body;
+      console.log(jobDetails);
 
-    app.post('/postJob', async(req, res)=>{
-        const jobDetails = req.body;
-        console.log(jobDetails)
-
-        const result = await jobCollection.insertOne(jobDetails);
-        res.send(result)
-    })
-
+      const result = await jobCollection.insertOne(jobDetails);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -54,14 +68,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Welcome to Job Portal Server");
+});
 
-
-app.get('/', (req, res)=>{
-    res.send("Welcome to Job Portal Server")
-})
-
-
-
-app.listen(port, ()=>{
-    console.log(`job portal server is running on port: ${port}`)
-})
+app.listen(port, () => {
+  console.log(`job portal server is running on port: ${port}`);
+});
